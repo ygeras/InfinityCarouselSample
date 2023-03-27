ccf77c1889edda57fef3ca83f2fa21f1e26512e8
ygeras <86482316+Yus312@users.noreply.github.com>
2023-03-27 15:50:59 +0300

    Initial Commit

---
+++ InfinityCarouselSample/InfinityCarouselSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfinityCarouselSampleApp.swift
+//  InfinityCarouselSample
+//
+//  Created by Yuri Gerasimchuk on 27.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct InfinityCarouselSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
